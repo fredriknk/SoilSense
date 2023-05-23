40681ebc5a862f4c5454786f8ca4300be5744dd9
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2023-05-23 15:48:06 +0200

    Feat: First working rust dev

---
+++ CODE/nrf52840-dk/CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+(no changes)
+
+## [0.2.0] - 2021-10-01
+
+### Changed
+- Update dependencies
+- Remove cortex-m-rt from main dependencies
+- Allow Led and Button Pins to be released
+
+[Unreleased]: https://github.com/nrf-rs/nrf52840-dk/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/nrf-rs/nrf52840-dk/compare/v0.1.0...v0.2.0
---
+++ CODE/nrf52840-dk/examples/blinky.rs
@@ -0,0 +1,38 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use nb::block;
+
+use panic_halt as _;
+
+use nrf52840_dk_bsp::{
+    hal::{
+        prelude::*,
+        timer::{self, Timer},
+    },
+    Board,
+};
+
+#[entry]
+fn main() -> ! {
+    let mut nrf52 = Board::take().unwrap();
+
+    let mut timer = Timer::new(nrf52.TIMER0);
+
+    // Alternately flash the red and blue leds
+    loop {
+        nrf52.leds.led_2.enable();
+        delay(&mut timer, 250_000); // 250ms
+        nrf52.leds.led_2.disable();
+        delay(&mut timer, 2_000_000); // 1s
+    }
+}
+
+fn delay<T>(timer: &mut Timer<T>, cycles: u32)
+where
+    T: timer::Instance,
+{
+    timer.start(cycles);
+    let _ = block!(timer.wait());
+}
